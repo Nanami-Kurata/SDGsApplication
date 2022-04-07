2288dea4ee1047163f3576f504bf15b5c10ea284
Nanami Kurata <krtnnm20011031@gmail.com>
2022-04-07 16:00:42 +0900

    first commit

---
+++ SDGsApp/SDGsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SDGsApp
+//
+//  Created by 倉田七海 on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SDGsApp/SDGsAppUITests/SDGsAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SDGsAppUITestsLaunchTests.swift
+//  SDGsAppUITests
+//
+//  Created by 倉田七海 on 2022/04/07.
+//
+
+import XCTest
+
+class SDGsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
